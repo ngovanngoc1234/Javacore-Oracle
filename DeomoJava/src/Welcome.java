836b46dfa9a8aa340a9df49c4601d447ea03b66a
ngovanngoc2014 <ngovanngoc2014@gmail.com>
2021-06-06 13:16:01 +0700

    update gile demojava

---
+++ DeomoJava/src/Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println(".......thuc hanh hien thi xin chao trong man hinh console......... ");
+        String arrHelloWord = "hello Word";
+        System.out.println(arrHelloWord);
+        System.out.println("...................Demo for to java.....................");
+        int numberMax = 20;
+        for (int i = 0; i <= numberMax; i++) {
+            System.out.println(i);
+        }
+    }
+}
