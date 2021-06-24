20b538345eee36bae9e5511338a5c44673bc6da9
ngovanngoc1234 <ngovanngoc2014@gmail.com>
2021-06-24 14:09:31 +0700

    update page chapter 3.5

---
+++ DeomoJava/src/chapter3/operators/MathematicalFunctionsAndConstants.java
@@ -0,0 +1,14 @@
+package chapter3.operators;
+
+public class MathematicalFunctionsAndConstants {
+    public static void main(String[] args) {
+//        java có đầy đử các phép tính toán học ở trong bộ thư viện java.lang
+        double numberSquare = 9;
+        double square = Math.sqrt(numberSquare);
+        System.out.println("hàm tính căn bậc 2 ");
+        System.out.println("căn bậc 2 của vd a = " + square);
+
+        System.out.println("-----------------------------------------");
+
+    }
+}
